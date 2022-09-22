ec3071da977cb1f014cb8f64d99de8f2239f4758
PasaWQe <70545650+PasaWQe@users.noreply.github.com>
2022-09-22 12:07:15 +0200

    Add files via upload

---
+++ dono/gitdobot.js
@@ -0,0 +1,7 @@
+const gitdobot = (prefix) => {
+return`​𝐒𝐀𝐈 𝐏𝐑𝐀 𝐋𝐀 𝐂𝐔𝐑𝐈𝐎𝐒𝐎 𝐓𝐄𝐌 𝐍𝐀𝐃𝐀 𝐀𝐐𝐔𝐈 𝐍𝐀̃𝐎.
+𝐅𝐈𝐂𝐀 𝐀𝐓𝐄𝐍𝐓𝐎 𝐍𝐎 𝐂𝐀𝐍𝐀𝐋 𝐏𝐑𝐀 𝐏𝐄𝐆𝐀 𝐌𝐄𝐔 𝐆𝐈𝐓.
+⇛ https://youtube.com/channel/UChnVbF6T9qmTmyVUcRmlstQ`
+}
+
+exports.gitdobot = gitdobot
