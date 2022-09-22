80ad839c5a776bab30257650ca4bd0bf0535c446
PasaWQe <70545650+PasaWQe@users.noreply.github.com>
2022-09-22 12:05:41 +0200

    Add files via upload

---
+++ menus/infodono.js
@@ -0,0 +1,30 @@
+const infodono = (prefix, numerodono, NomeDoBot) => {
+
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
+
+return`
+╭━━━━━◉                           ◉━━━━━╮
+       ╔┉✵┉═══『ஓீ͜ ፝👑ू ፝͜❥ 』═══┉✵┉╗    
+       ║       *喜𝑰𝑵𝑭𝑶 𝑫𝑶𝑵𝑶夢*                 
+       ╚┉✵┉═══『ஓீ͜ ፝🤴🏻ू ፝͜❥ 』═══┉✵┉╝    
+╰━━━━━◉                           ◉━━━━━╯
+
+╭═══════◉
+┆      ╔┉✵┉═══『🤴🏻 』═══┉✵┉╗
+┆      ║
+┆      ║⏤͟͟͞͞⿻͜͡👑︎ Nome: ${NickDono}
+┆      ║⏤͟͟͞͞⿻͜͡👑︎ Proprietário: wa.me/${numerodn}
+┆      ║⏤͟͟͞͞⿻͜͡👑 ︎Prefix: ${prefix}
+┆      ║
+┆      ╚┉✵┉═══『🤴🏻 』═══┉✵┉╝
+╰═══════◉
+╭━━━━━◉
+       ╔┉✵┉═══『ஓீ͜ ፝🧸ू ፝͜❥ 』═══┉✵┉╗    
+       ║      *喜${NomeDoBot}夢*                 
+       ╚┉✵┉═══『ஓீ͜ ፝🧸ू ፝͜❥ 』═══┉✵┉╝ 
+╰━━━━━◉
+`
+}
+
+exports.infodono = infodono
